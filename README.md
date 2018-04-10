036d8a43871a06f0fb1dbf34028b722c25fbc7a5
datle2011@gmail.com <dat@proteusion.com>
2018-04-10 17:42:29 +0700

    init master

---
+++ README.md
@@ -0,0 +1,24 @@
+# Laravel Tag
+Add tags to model
+
+## Install
+
+* **Thêm vào file composer.json của app**
+```json
+	"repositories": [
+        {
+            "type": "vcs",
+            "url": "https://github.com/datlv/laravel-tag"
+        }
+    ],
+    "require": {
+        "datlv/laravel-tag": "dev-master"
+    }
+```
+``` bash
+$ composer update
+```
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
---
+++ src/ServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Datlv\Tag;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+/**
+ * Class ServiceProvider
+ *
+ * @package Datlv\Tag
+ */
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     */
+    public function boot()
+    {
+        $this->loadTranslationsFrom(__DIR__ . '/../lang', 'tag');
+        $this->loadViewsFrom(__DIR__ . '/../views', 'tag');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->publishes(
+            [
+                __DIR__ . '/../views' => base_path('resources/views/vendor/tag'),
+                __DIR__ . '/../lang' => base_path('resources/lang/vendor/tag'),
+                __DIR__ . '/../config/tag.php' => config_path('tag.php'),
+            ]
+        );
+        if ($this->app->has('layout')) {
+            app('layout')->registerWidgetTypes(config('tag.widgets'));
+        }
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/tag.php', 'tag');
+    }
+}
